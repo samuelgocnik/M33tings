b513c15c7f809875276c6dc79219ced6c456141c
Samuel Gocník <xgocnik@fi.muni.cz>
2021-12-28 21:12:38 +0100

    feat: session, hashing

--- server/dist/app.js
+++ server/dist/app.js
@@ -6,11 +6,38 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const pg_1 = require("pg");
 const cors_1 = __importDefault(require("cors"));
+const bcrypt_1 = __importDefault(require("bcrypt"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const express_session_1 = __importDefault(require("express-session"));
+const saltRounds = 10;
 const app = (0, express_1.default)();
 const port = 4000;
 // automatically parse every sent object
 app.use(express_1.default.json());
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    credentials: true,
+}));
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+app.use((0, cookie_parser_1.default)());
+//important line, always put it here, otherwise it won't work
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use((0, express_session_1.default)({
+    name: 'user',
+    secret: 'thisismysecrctekeyfhrgfgrfrty84fw2i3ohfdhibenqdownmcwncir767',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        sameSite: 'none',
+        // secure: true,   // uncomment for production
+    },
+}));
 const db = new pg_1.Pool({
     user: 'postgres',
     password: 'postgres',
@@ -19,36 +46,106 @@ const db = new pg_1.Pool({
     port: 5432,
     connectionTimeoutMillis: 2000,
 });
-console.log(db.totalCount, db.idleCount);
 app.get('/', (req, res) => {
     res.json({ message: 'Hello consumer!' });
+});
+app.get('/logout', (req, res) => {
+    if (req.session.user) {
+        req.session.destroy((err) => {
+            if (err) {
+                return res.send({ message: 'Logout error' });
+            }
+            req.session = null;
+            res.clearCookie('user', { path: '/' });
+            res.send({ clearSession: 'success' });
+        });
+    }
+    else {
+        res.status(404).json();
+    }
+});
+app.get('/login', (req, res) => {
+    if (req.session.user) {
+        console.log('logged');
+        res.json({
+            loggedIn: true,
+            user: {
+                id: req.session.user.id,
+                name: req.session.user.name,
+                picture: req.session.user.picture,
+                created_at: req.session.user.created_at,
+            },
+        });
+    }
+    else {
+        console.log('not logged');
+        res.json({ loggedIn: false, user: null });
+    }
 });
 app.post('/login', (req, res) => {
     const username = req.body.username;
     const pwd = req.body.pwd;
-    db.query('SELECT * FROM users WHERE name = $1 AND pwd = $2', [username, pwd], (error, result) => {
+    db.query('SELECT * FROM users WHERE name = $1', [username], (error, result) => {
         if (error) {
-            res.status(404).json({ error });
+            res.status(404).json(error);
             return;
         }
         if (result.rowCount > 0) {
-            res.json(result);
+            bcrypt_1.default.compare(pwd, result.rows[0].pwd, (err, same) => {
+                if (err) {
+                    res.status(404).json(err);
+                    return;
+                }
+                if (same) {
+                    // create user's session
+                    req.session.user = {
+                        id: result.rows[0].id,
+                        name: result.rows[0].name,
+                        picture: result.rows[0].picture,
+                        created_at: result.rows[0].created_at,
+                    };
+                    res.json(req.session.user);
+                }
+                else {
+                    res.json({ message: 'Wrong password!' });
+                }
+            });
         }
         else {
-            res.json({ message: 'Wrong name or password!' });
+            res.json({ message: "User doesn't exist!" });
         }
     });
 });
+// returning ID of newly registered user
+// in db has to be at least one user for correct working
 app.post('/register', (req, res) => {
-    console.log('register!!');
     const username = req.body.username;
     const pwd = req.body.pwd;
-    db.query('INSERT INTO users (name, pwd) VALUES ($1, $2)', [username, pwd], (error, result) => {
+    bcrypt_1.default.hash(pwd, saltRounds, (error, hash) => {
         if (error) {
-            res.status(404).json({ error });
+            res.status(404).json(error);
             return;
         }
-        res.json(result);
+        db.query('INSERT INTO users (name, pwd) ' +
+            'SELECT $1, $2 ' +
+            'FROM users ' +
+            'WHERE NOT EXISTS( ' +
+            'SELECT id ' +
+            'FROM users ' +
+            'WHERE name = $1 ' +
+            ') ' +
+            'LIMIT 1', [username, hash], (err, result) => {
+            if (err) {
+                res.status(404).json(err);
+                return;
+            }
+            if (result.rowCount == 0) {
+                res.json({ message: 'User already exists!', result: result });
+            }
+            else {
+                res.status(201).json(result);
+            }
+        });
     });
 });
 app.get('/events', (req, res) => {
--- server/src/app.ts
+++ server/src/app.ts
@@ -2,14 +2,56 @@ import express, { Request, Response } from 'express';
 import { Pool } from 'pg';
 import cors from 'cors';
 import bcrypt from 'bcrypt';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
 
 const saltRounds = 10;
 const app = express();
 const port = 4000;
 
+declare module 'express-session' {
+  export interface SessionData {
+    user: { [key: string]: {} | null };
+  }
+}
+
 // automatically parse every sent object
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  })
+);
+
+app.use(function (req: Request, res: Response, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+app.use(cookieParser());
+//important line, always put it here, otherwise it won't work
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    name: 'user',
+    secret: 'thisismysecrctekeyfhrgfgrfrty84fw2i3ohfdhibenqdownmcwncir767', // a random unique string key used to authenticate a session, Can't be exposed to the public
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      sameSite: 'none',
+      // secure: true,   // uncomment for production
+      maxAge: 1000 * 60 * 60 * 12, // lasts for 12h, default is 'session' -> cookie will last until closing browser etc.
+    },
+  })
+);
 
 const db = new Pool({
   user: 'postgres',
@@ -20,10 +62,41 @@ const db = new Pool({
   connectionTimeoutMillis: 2000,
 });
 
-console.log(db.totalCount, db.idleCount);
-
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Hello consumer!' });
+});
+
+app.get('/logout', (req: Request, res: Response) => {
+  if (req.session.user) {
+    req.session.destroy((err) => {
+      if (err) {
+        return res.send({ message: 'Logout error' });
+      }
+      req.session = null;
+      res.clearCookie('user', { path: '/' });
+      res.send({ clearSession: 'success' });
+    });
+  } else {
+    res.status(404).json();
+  }
+});
+
+app.get('/login', (req: Request, res: Response) => {
+  if (req.session.user) {
+    console.log('logged');
+    res.json({
+      loggedIn: true,
+      user: {
+        id: req.session.user.id,
+        name: req.session.user.name,
+        picture: req.session.user.picture,
+        created_at: req.session.user.created_at,
+      },
+    });
+  } else {
+    console.log('not logged');
+    res.json({ loggedIn: false, user: null });
+  }
 });
 
 app.post('/login', (req: Request, res: Response) => {
@@ -31,42 +104,73 @@ app.post('/login', (req: Request, res: Response) => {
   const pwd = req.body.pwd;
 
   db.query(
-    'SELECT * FROM users WHERE name = $1 AND pwd = $2',
-    [username, pwd],
+    'SELECT * FROM users WHERE name = $1',
+    [username],
     (error, result) => {
       if (error) {
-        res.status(404).json({ error });
+        res.status(404).json(error);
         return;
       }
+
       if (result.rowCount > 0) {
-        res.json(result.rows);
+        bcrypt.compare(pwd, result.rows[0].pwd, (err, same) => {
+          if (err) {
+            res.status(404).json(err);
+            return;
+          }
+          if (same) {
+            // create user's session
+            req.session.user = {
+              id: result.rows[0].id,
+              name: result.rows[0].name,
+              picture: result.rows[0].picture,
+              created_at: result.rows[0].created_at,
+            };
+            res.json(req.session.user);
+          } else {
+            res.json({ message: 'Wrong password!' });
+          }
+        });
       } else {
-        res.json({ message: 'Wrong name or password!' });
+        res.json({ message: "User doesn't exist!" });
       }
     }
   );
 });
 
+// returning ID of newly registered user
+// in db has to be at least one user for correct working
 app.post('/register', (req: Request, res: Response) => {
   const username = req.body.username;
   const pwd = req.body.pwd;
 
-  bcrypt.hash(pwd, saltRounds, (err, hash) => {
-    if (err) {
-      console.log(err);
-      res.status(404).json({ message: 'Hash error occured' });
+  bcrypt.hash(pwd, saltRounds, (error, hash) => {
+    if (error) {
+      res.status(404).json(error);
       return;
     }
 
     db.query(
-      'INSERT INTO users (name, pwd) VALUES ($1, $2)',
+      'INSERT INTO users (name, pwd) ' +
+        'SELECT $1, $2 ' +
+        'FROM users ' +
+        'WHERE NOT EXISTS( ' +
+        'SELECT id ' +
+        'FROM users ' +
+        'WHERE name = $1 ' +
+        ') ' +
+        'LIMIT 1',
       [username, hash],
-      (error, result) => {
-        if (error) {
-          res.status(404).json({ error });
+      (err, result) => {
+        if (err) {
+          res.status(404).json(err);
           return;
         }
-        res.json(result);
+        if (result.rowCount == 0) {
+          res.json({ message: 'User already exists!', result: result });
+        } else {
+          res.status(201).json(result);
+        }
       }
     );
   });
