3ae2da598fd4ad0e8104a3ea1ebb4c50d08ce937
Samuel Gocník <xgocnik@fi.muni.cz>
2022-01-10 23:49:15 +0100

    feat: added datetime picker

--- client/src/components/Events/NewEvent.tsx
+++ client/src/components/Events/NewEvent.tsx
@@ -1,9 +1,17 @@
-import React, { Fragment, useRef } from 'react';
+import React, { Fragment, useRef, useState } from 'react';
 import Button from '../UI/Button';
 import Input from '../UI/Input/Input';
+import DateTimePicker from 'react-datetime-picker';
 
 const NewEvent = () => {
   const nameInputRef = useRef<HTMLInputElement>(null);
+  const [date, setDate] = useState<Date>(new Date());
+
+  // const dateOnChangeHandler = (event: {
+  //   target: { value: React.SetStateAction<Date> };
+  // }) => {
+  //   setDate(event.target.value);
+  // };
 
   const submitFormHandler = (event: React.FormEvent) => {
     event.preventDefault();
@@ -19,9 +27,10 @@ const NewEvent = () => {
           id="name"
           label="Name of the event"
           type="text"
-          error_message='Enter a valid event name (at least 4 chars long)'
+          error_message="Enter a valid event name (at least 4 chars long)"
           validate={(data: string): boolean => data.trim().length > 4}
         />
+        <DateTimePicker onChange={date => setDate(date)} value={date} />
         <Button
           type="submit"
           text="Add event"
